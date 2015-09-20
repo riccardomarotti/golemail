3cf4ea4962c5e4ab3f6deb96d066704b4ad27929
Riccardo <riccardo.marotti@gmail.com>
2015-09-20 11:19:36 +0200

    Added some logic to recognize timing words.

--- parsing-test.rkt
+++ parsing-test.rkt
@@ -1,16 +1,27 @@
 #lang racket/base
 
 (require rackunit
-	 rackunit/text-ui
+         rackunit/text-ui
          "parsing.rkt")
 
 (define all
-	(test-suite
-		"subject parsing"
-		(test-equal? "empty string" (should-parse? "") #f)
-		(test-equal? "string with no keyword" (should-parse? "any string") #f)
-		(test-equal? "string with keyword not as last" (should-parse? ".>>> and anything else") #f)
-		(test-equal? "string with keyword as last" (should-parse? "anything ending with .>>>") #t)))
+  (list
+    (test-suite
+      "subject parsing"
+      (test-equal? "empty string" (should-parse? "") #f)
+      (test-equal? "string with no keyword" (should-parse? "any string") #f)
+      (test-equal? "string with keyword not as last" (should-parse? ".>>> and anything else") #f)
+      (test-equal? "string with keyword as last" (should-parse? "anything ending with .>>>") #t))
 
+    (test-suite
+      "finding timing expressions"
+      (check-false (contains-timing? ""))
+      (check-false (contains-timing? "any non timing string"))
+      (check-true (contains-timing? "any text tra un'ora"))
+      (check-true (contains-timing? "any text alle 10"))
+      (check-true (contains-timing? "any text il 10 settembre"))
+      (check-true (contains-timing? "any text lunedì"))
+      (check-true (contains-timing? "any text MercolEdi"))
+      )))
 
-(run-tests all)
+(map run-tests all)
--- parsing.rkt
+++ parsing.rkt
@@ -1,11 +1,38 @@
 #lang racket
 
 (define (should-parse? string_to_parse)
-	(define list (string->list string_to_parse))
-	(cond
-		[(empty? list) #f]
-		[(string=? ".>>>" string_to_parse) #t]
-		[else (should-parse? (list->string (rest list)))]))
+  (define list (string->list string_to_parse))
+  (cond
+    [(empty? list) #f]
+    [(string=? ".>>>" string_to_parse) #t]
+    [else (should-parse? (list->string (rest list)))]))
+
+(define timing-words
+  (list
+    "tra"
+    "alle"
+    "il"
+    "lunedì"
+    "lunedi"
+    "martedì"
+    "martedi"
+    "mercoledì"
+    "mercoledi"
+    "giovedì"
+    "giovedi"
+    "venerdì"
+    "venerdi"
+    "sabato"
+    "domenica"))
+
+(define (contains-timing? string)
+	(contains-timing?recursive (reverse (string-split string))))
+
+(define (contains-timing?recursive words)
+  (cond
+  	[(empty? words) #f]
+  	[(not (equal? #f (member (string-downcase (first words)) timing-words))) #t]
+  	[else (contains-timing?recursive (rest words))]))
 
 
-(provide should-parse?)
+(provide should-parse? contains-timing?)
