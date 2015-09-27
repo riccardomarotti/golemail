718c3520b79c8eb6c696def26742071c8d5773b8
Riccardo <riccardo.marotti@gmail.com>
2015-09-27 16:46:01 +0200

    removed logic from parser

--- parsing-test.rkt
+++ parsing-test.rkt
@@ -4,32 +4,8 @@
          rackunit/text-ui
          "parsing.rkt")
 
-(require/expose "parsing.rkt" (extract-hour extract-minute extract-second))
-
 (define all
   (list
-   (test-suite
-    "time extraction"
-    (test-case
-     "extract-hour"
-     (define current-date (seconds->date 1443191395));"Friday, September 25th, 2015 4:29:55pm"
-
-     (check-equal? (extract-hour "any string" current-date) 16)
-     (check-equal? (extract-hour "10:30" current-date) 10)
-     (check-equal? (extract-hour "23" current-date) 23)
-     (check-equal? (extract-hour "32  minuti" current-date) 16))
-
-    (test-case
-     "extract-minute"
-     (define current-date (seconds->date 1443191395));"Friday, September 25th, 2015 4:29:55pm"
-
-     (check-equal? (extract-minute "any non timing string" current-date) 0)
-     (check-equal? (extract-minute "10:30" current-date) 30)
-     (check-equal? (extract-minute "9" current-date) 0)
-     (check-equal? (extract-minute "tra 5 minuti" current-date) 34)
-
-     (check-equal? (extract-second "any string") 0)))
-
    (test-suite
     "subject parsing"
     (test-equal? "empty string" (get-reminder-string "") #f)
@@ -43,72 +19,19 @@
     (check-false (extract-timing "any non timing string"))
     (check-equal? (extract-timing "any text tra 1 ora") "tra 1 ora")
     (check-equal? (extract-timing "any text tra 10 ore") "tra 10 ore")
+    (check-equal? (extract-timing "any text tra 10 giorni") "tra 10 giorni")
+    (check-equal? (extract-timing "any text tra 1 giorno") "tra 1 giorno")
     (check-equal? (extract-timing "any text tra 7 minuti") "tra 7 minuti")
+    (check-equal? (extract-timing "any text tra 1 minuto") "tra 1 minuto")
     (check-equal? (extract-timing "any text alle 10") "alle 10")
+    (check-equal? (extract-timing "any text alle 10:30") "alle 10:30")
     (check-equal? (extract-timing "any text il 10 settembre") "il 10 settembre")
     (check-equal? (extract-timing "any text lunedì") "lunedì")
-    (check-equal? (extract-timing "any text MercolEdi") "MercolEdi")
+    (check-equal? (extract-timing "any text MercolEdi") "mercoledi")
     (check-equal? (extract-timing "any text oggi") "oggi")
-    (check-equal? (extract-timing "any text domaNi") "domaNi")
+    (check-equal? (extract-timing "any text domaNi") "domani")
     (check-equal? (extract-timing "any text sabato alle 17") "sabato alle 17"))
-
-   (test-suite
-    "understanding timing expressions"
-    (test-case
-     "no timing string"
-     (check-equal? (date-of-reminder "" "any date") #f)
-     (check-equal? (date-of-reminder "any non timing string" "any date") #f))
-
-    (test-case
-     "today"
-     (define current-date (seconds->date 1442851540)) ;"Monday, September 21st, 2015 6:05:40pm"
-
-     (define 17time (date-of-reminder "oggi alle 17" current-date))
-     (check-equal? (date-hour 17time) 17)
-     (check-equal? (date-minute 17time) 0)
-     (check-equal? (date-second 17time) 0)
-     (check-equal? (date-week-day 17time) 1)
-     (check-equal? (date-day 17time) 21)
-     (check-equal? (date-month 17time) 9)
-     (check-equal? (date-year 17time) 2015)
-
-     (define 22time (date-of-reminder "oggi alle 22" current-date))
-     (check-equal? (date-hour 22time) 22))
-
-    (test-case
-     "tomorrow"
-     (define current-date (seconds->date 1442948973)) ;"Tuesday, September 22nd, 2015 9:09:33pm"
-
-     (define a-time (date-of-reminder "domani alle 15" current-date))
-     (check-equal? (date-hour a-time) 15)
-     (check-equal? (date-minute a-time) 0)
-     (check-equal? (date-second a-time) 0)
-     (check-equal? (date-week-day a-time) 3)
-     (check-equal? (date-day a-time) 23))
-
-    (test-case
-     "more precise time"
-     (define current-date (seconds->date 1443006675)) ;"Wednesday, September 23rd, 2015 1:11:15pm"
-
-     (define calculated-time (date-of-reminder "alle 12:37" current-date))
-     (check-equal? (date-hour calculated-time) 12)
-     (check-equal? (date-minute calculated-time) 37)
-     (check-equal? (date-second calculated-time) 0)
-     (check-equal? (date-day calculated-time) 23))
-
-    (test-case
-     "in X hours/minutes"
-     (define current-date (seconds->date 1443037023)) ;"Wednesday, September 23rd, 2015 9:37:48pm"
-     (define calculated-time (date-of-reminder "tra 12 minuti" current-date))
-     (check-equal? (date-hour calculated-time) 21)
-     (check-equal? (date-minute calculated-time) 49)
-     (check-equal? (date-day calculated-time) 23)
-
-     (define calculated-time2 (date-of-reminder "tra 23 minuti" current-date))
-     (check-equal? (date-hour calculated-time2) 22)
-     (check-equal? (date-minute calculated-time2) 0)
-     )
-    )))
+   ))
 
 (for-each run-tests all)
   ; (run-tests (car all))
--- parsing.rkt
+++ parsing.rkt
@@ -1,22 +1,16 @@
 #lang racket
 
-(require racket/date)
-
-(define (get-reminder-string string_to_parse)
-  (define result (regexp-match "(.*)\\.>>>$" string_to_parse))
-  (and result (second result)))
-
 (define week-day-regexp
   "(?i:luned[iì]|marted[iì]|mercoled[iì]|gioved[iì]|venerd[iì]|sabato|domenica|oggi|domani|dopodomani)")
 
 (define hour-regexp "(?:([0-2]?[0-9]):)?([0-5]?[0-9])(?!.*minut[io])")
-(define minute-regexp "[0-2]?[0-9]:([0-5]?[0-9])|([0-5]?[0-9])(?: *minut[io])")
 (define month-regexp "(?i:gennaio|febbraio|marzo|aprile|maggio|giugno|luglio|agosto|settembre|ottobre|novembre|dicembre)")
 
 (define timing-regexps
   (list
-   "tra +([0-9]+) +minuti *$"
+   "tra +([0-9]+) +minut[io] *$"
    "tra +([0-9]+) +or[ae] *$"
+   "tra +([0-9]+) +giorn[io] *$"
    (~a week-day-regexp " +alle +" hour-regexp)
    (~a "alle +" hour-regexp)
    (~a "il +([0-9]+) +" month-regexp)
@@ -25,55 +19,14 @@
 (define timing-regexp (string-join timing-regexps "|"))
 
 
+
+(define (get-reminder-string string_to_parse)
+  (define result (regexp-match "(.*)\\.>>>$" string_to_parse))
+  (and result (second result)))
+
 (define (extract-timing string)
   (define result (regexp-match timing-regexp string))
-  (and result (first result)))
-
-(define (date-of-reminder reminder current-date)
-  (if (extract-timing reminder)
-      (struct-copy date current-date
-                   [hour (extract-hour reminder current-date)]
-                   [minute (extract-minute reminder current-date)]
-                   [second (extract-second reminder)]
-                   [week-day (extract-week-day reminder current-date)]
-                   [day (extract-day reminder current-date)])
-    #f ))
-
-(define (extract-hour reminder current-date)
-  (define matches (regexp-match hour-regexp reminder))
-  (if matches
-    (string->number (first (remove* '(#f) (rest matches))))
-    (date-hour current-date)))
+  (and result (string-downcase (first result))))
 
 
-(define (extract-minute reminder current-date)
-  (define matches (regexp-match minute-regexp reminder))
-  (define operation
-    (cond
-     [(regexp-match ".*tra.*" reminder) (λ(x y) (remainder (+ x y) 60))]
-     [else (λ(x y) y)]))
-
-  (if matches (operation (date-minute current-date) (string->number (last (remove* '(#f) matches)))) 0))
-
-(define (extract-second reminder)
-  0)
-
-(define (extract-week-day reminder current-date)
-  (define matches (regexp-match week-day-regexp reminder))
-  (define day (and matches (first matches)))
-  (cond
-   [(equal? day "domani") (add1 (date-week-day current-date))]
-   [else (date-week-day current-date)]))
-
-(define (extract-day reminder current-date)
-  (define matches (regexp-match week-day-regexp reminder))
-  (define day (and matches (first matches)))
-  (cond
-   [(equal? day "domani") (add1 (date-day current-date))]
-   [else (date-day current-date)]))
-
-
-
-
-
-(provide get-reminder-string extract-timing date-of-reminder)
+(provide get-reminder-string extract-timing)
