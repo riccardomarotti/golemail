812bbc2ce9086669c61a0676abae329f9957454c
Riccardo Marotti <riccardo@marotti.xyz>
2015-10-08 13:28:27 +0200

    refactor

---
+++ configuration.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(require json
+         net/imap)
+
+(define (read-config)
+  (define config-in (open-input-file "config.json" #:mode 'text))
+  (define config (read-json config-in))
+  (close-input-port config-in)
+
+  (imap-port-number (hash-ref config 'port))
+
+  (list
+    (hash-ref config 'server)
+    (hash-ref config 'username)
+    (hash-ref config 'password)
+    (hash-ref config 'tag)))
+
+(define (server)
+    (first (read-config)))
+
+(define (username)
+    (second (read-config)))
+
+(define (tag)
+    (fourth (read-config)))
+
+(define (password)
+    (third (read-config)))
+
+
+
+(provide server
+         username
+         password
+         tag)
---
+++ imap-access.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(require net/imap
+         "structures.rkt"
+         "configuration.rkt")
+
+
+(define (mark-as status connection messages-positions)
+  (imap-store connection '+ messages-positions (list (symbol->imap-flag status))))
+
+(define (move-messages-to mailbox messages from)
+  (define-values (connection anything anything-else) (connect from))
+  (define messages-positions (map (λ(message) (message-position message)) messages))
+  (imap-copy connection messages-positions mailbox)
+  (mark-as 'deleted connection messages-positions)
+  (imap-expunge connection))
+
+(define (get-new-messages connection messages recent-messages-count)
+  (define messages-positions (map add1 (range messages)))
+  (map (λ(message-and-uid message-number)
+          (message (first message-and-uid) "Inbox" (second message-and-uid) message-number))
+       (imap-get-messages connection
+                          messages-positions
+                          '(header uid)) messages-positions))
+
+(define (connect mailbox)
+  (imap-connect
+   (server)
+   (username)
+   (password)
+   mailbox
+   #:tls? #t))
+
+(define (get-messages mailbox)
+    (define-values (connection messages newmessages) (connect mailbox))
+    (get-new-messages connection messages newmessages))
+
+
+(provide get-messages
+         move-messages-to)
