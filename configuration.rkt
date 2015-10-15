a5ec64c176ec0078939542cf31cbbfade58f6afb
Riccardo <riccardo.marotti@gmail.com>
2015-10-15 19:59:47 +0200

    now polling interval is read from configuration

--- configuration.rkt
+++ configuration.rkt
@@ -7,14 +7,15 @@
   (define config-in (open-input-file "config.json" #:mode 'text))
   (define config (read-json config-in))
   (close-input-port config-in)
-  
+
   (imap-port-number (hash-ref config 'port))
-  
+
   (list
    (hash-ref config 'server)
    (hash-ref config 'username)
    (hash-ref config 'password)
-   (hash-ref config 'tag)))
+   (hash-ref config 'tag)
+   (hash-ref config 'polling-interval)))
 
 (define (server)
   (first (read-config)))
@@ -22,15 +23,19 @@
 (define (username)
   (second (read-config)))
 
+(define (password)
+  (third (read-config)))
+
 (define (tag)
   (fourth (read-config)))
 
-(define (password)
-  (third (read-config)))
+(define (polling-interval)
+  (fifth (read-config)))
 
 
 
 (provide server
          username
          password
-         tag)
+         tag
+         polling-interval)
